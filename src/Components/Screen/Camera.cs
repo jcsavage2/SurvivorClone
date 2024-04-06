aecf9fb8cdbd7c5a3d7f82b1502f18f82f51ea5b
jcsavage <jcsavage@umich.edu>
2024-04-06 15:00:58 -0700

    Clamp camera to map bounds

--- src/Components/Screen/Camera.cs
+++ src/Components/Screen/Camera.cs
@@ -6,23 +6,27 @@ public class Camera
 {
   public Matrix translation { get; private set; }
 
+  // Update camera position based on player position, clamp to map boundaries
   public void Update(RenderManager _renderManager, Player _player, Map _map)
   {
-    var cameraTranslationVector =
-      new Vector3(-_player.GetPosition().X, -_player.GetPosition().Y, 0) * new Vector3(_renderManager.GetRatioX(), _renderManager.GetRatioY(), 0f); // Origin from player position
-    var resTranslationVector = new Vector3(_renderManager.GetRenderSize().X / 2, _renderManager.GetRenderSize().Y / 2, 0); // Offset from origin to center the player in the middle of the screen
+    var playerPosition =
+      new Vector3(_player.GetPosition().X, _player.GetPosition().Y, 0) * new Vector3(_renderManager.GetRatioX(), _renderManager.GetRatioY(), 0f);
+    var renderSize = _renderManager.GetRenderSize();
+
+    var minX = renderSize.X / 2 - 32;
+    var maxX = _map.GetMapDimensionsPixels().X - renderSize.X / 2 - 32;
+    var minY = renderSize.Y / 2 - 32;
+    var maxY = _map.GetMapDimensionsPixels().Y - renderSize.Y / 2 - 32;
+
+    var clampedX = MathHelper.Clamp(playerPosition.X, minX, maxX);
+    var clampedY = MathHelper.Clamp(playerPosition.Y, minY, maxY);
+
+    var cameraTranslationVector = new Vector3(-clampedX, -clampedY, 0);
+    var resTranslationVector = new Vector3(renderSize.X / 2, renderSize.Y / 2, 0);
 
     var cameraTranslationMatrix = Matrix.CreateTranslation(cameraTranslationVector);
     var resTranslationMatrix = Matrix.CreateTranslation(resTranslationVector);
 
     translation = cameraTranslationMatrix * resTranslationMatrix;
-
-    // var ratioX = RenderManager.WindowSize.X / RenderManager.RenderSize.X;
-    // var ratioY = RenderManager.WindowSize.Y / RenderManager.RenderSize.Y;
-    // var dx = (RenderManager.WindowSize.X / 2) - player.position.X * ratioX;
-    // dx = MathHelper.Clamp(dx, -map.mapDimensionsPixels.X + RenderManager.WindowSize.X + (map.tileSize / 2), map.tileSize / 2f);
-    // var dy = (RenderManager.WindowSize.Y / 2) - player.position.Y * ratioY;
-    // dy = MathHelper.Clamp(dy, -map.mapDimensionsPixels.Y + RenderManager.WindowSize.Y + (map.tileSize / 2), map.tileSize / 2f);
-    // translation = Matrix.CreateTranslation(dx, dy, 0f);
   }
 }
